84b90ca0c31de39744902b9a4340130ba9703aa7
EsterHajos <ester.marta.hajos@gmail.com>
2026-03-16 11:20:14 +0100

    exersices done and added

---
+++ assignment1.js
@@ -0,0 +1,43 @@
+
+// exersice 1 //
+console.log ("Hello World");
+
+
+//exersice 2// 
+let firstName;
+let age;
+let favoriteColor;
+
+
+firstName = "Ester";
+age = 32;
+favoriteColor = "Green";
+
+console.log(firstName);
+console.log(age);
+console.log(favoriteColor);
+
+
+//exersice 3//
+let children;
+let adults;
+
+children = 10;
+adults = 2; 
+
+children = children + 2;
+children = children - 2;
+adults = adults * 5;
+adults= adults / 5; 
+
+
+console.log(children);
+console.log(adults);
+
+
+
+
+
+
+
+
