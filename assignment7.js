5d871d39f40dd95efc9681e40b7e8253e61655da
EsterHajos <ester.marta.hajos@gmail.com>
2026-04-20 13:42:11 +0200

    started js 6 and 7 created file and added instructions

---
+++ assignment7.js
@@ -0,0 +1,14 @@
+// Assignment lesson JS7: Document Object Model  
+
+
+//PART 1  
+//Create a addWordToDOM function that will update the ”word” element with a random item from the words array  
+//Create a updateScore function that will increment score by +1  
+//Add an event listener to the ”text” element. When you type in the correct word, the function should:   
+//Call updateScore   
+//give the user a new word by calling addWordToDOM  
+//increment time by 5 seconds   
+//reset the input to empty string  
+//PART 2  
+//Create a updateTime function using the setInterval( ) method, every time it runs it should decrement -1 from the timer. Stop the timer when it reaches zero.  
+//Create  a gameOver function that will display the end-game-container once the timer hits zero  
