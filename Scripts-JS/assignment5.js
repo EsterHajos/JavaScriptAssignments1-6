804c47fda136caa0d2f268ea99913cb4e0c50c5a
EsterHajos <ester.marta.hajos@gmail.com>
2026-05-13 12:26:10 +0200

    Update assignment5.js

--- Scripts-JS/assignment5.js
+++ Scripts-JS/assignment5.js
@@ -77,16 +77,23 @@ function runATM() {
             "Withdrawal\n" +
             "Name\n" +
             "Exit"
-        );
+        ).toLowerCase();
 
-        if (action === "Exit") {
+        if (action === "exit") {
             running = false;
             alert("Thanks for using the ATM!");
+            continue;   
 
-        } else if (action === "Deposit" || action === "Withdrawal") {
+        } 
+        
+        if (action === "Deposit" || action === "Withdrawal") {
             let amount = Number(prompt("Enter amount:"));
             alert(atm(action, amount));
         } 
+
+        else {
+            alert(atm(action));
+        }
 
     }
 }
@@ -109,7 +116,7 @@ runATM();
 
 
 
-        =
+        
 console.log(atm("name"));
 console.log(atm("balance"));
 console.log(atm("deposit", 500));
@@ -148,18 +155,3 @@ console.log(atm("balance"));
 
 
 
-
-
-
-
-
-
-
-//Currently, your code works, but nothing is stored since the ATM function only runs once. That means that right now,
- //if money is deposited or withdrawn, this is not reflected in the balance 
- //since the user needs to refresh the page to make another choice. To fix this, 
- //you can put the switch case in a while loop. This will allow the user to make multiple choices that build on each previous choice  
-//You have not included the user interaction in your ATM function. 
-//In the instructions, it says that you need to give the user a choice 
-//of what menu option to choose and to then run the correct part of the code according to that choice. 
-//This should be done through a prompt  
